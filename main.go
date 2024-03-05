10ec0e16ea29df68d543fea1a540ece2fbe99531
haroune213 <123390922+Haroune213@users.noreply.github.com>
2024-03-05 17:45:27 -0500

    creating the htmx messaging template

--- main.go
+++ main.go
@@ -1,13 +1,19 @@
 package main
 
 import (
-	"log"
+	"fmt"
 	"messagingApp/routes"
 	"net/http"
 )
 
 func main() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		// Serve the HTML page
+		http.ServeFile(w, r, "./templates/index.html")
+	})
 
+	// Start the HTTP server on port 8080
+	fmt.Println("Server is running on port 8080")
+	http.ListenAndServe(":8080", nil)
 	http.HandleFunc("/web", routes.WebSocket)
-	log.Fatal(http.ListenAndServe(":8008", nil))
 }
